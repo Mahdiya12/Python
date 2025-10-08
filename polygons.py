8da2d8666a3d31ef5abc29a85bd80805b8db9263
Mahdiya12 <mmahnoorshooter@gmail.com>
2025-10-08 22:10:28 +0600

    triangle and sqaure

---
+++ polygons.py
@@ -0,0 +1,31 @@
+import turtle
+
+turtle.Screen().bgcolor("light coral")
+
+board = turtle.Turtle()
+
+#tiragle
+board.forward(100)
+board.left(120)
+
+board.forward(100)
+board.left(120)
+
+board.forward(100)
+
+#sqaure
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+turtle.done()
+

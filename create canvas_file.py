313bb9d12b70a624f01c02dbcc71ca37dd97bdea
Mahdiya12 <mmahnoorshooter@gmail.com>
2025-10-08 22:00:08 +0600

    python data typecasting

---
+++ create canvas_file.py
@@ -0,0 +1,11 @@
+import turtle
+
+turtle.Screen().bgcolor("light coral")
+
+sc = turtle.Screen()
+
+sc.setup(400, 300)
+
+turtle.title("welcome to the turtle")
+
+turtle.done()
---
+++ datatype_typecasting.py
@@ -0,0 +1,21 @@
+name = "Mahdiya"
+age = 12
+is_student = True
+weight = 40.5
+
+print("Name: ", name)
+print("Data Type of name", type(name))
+print("Age: ", age)
+print("Data Type of age", type(age))
+print("is_student: ", is_student)
+print("Data Type of is_student", type(is_student))
+print("Weight: ", weight)
+print("Data Type of weight", type(weight))
+
+print("/n After Type Casting....")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is", type(weight))

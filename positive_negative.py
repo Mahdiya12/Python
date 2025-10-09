92ded8283f71392fc455867a013db1c5e05466a4
Mahdiya12 <mmahnoorshooter@gmail.com>
2025-10-09 21:42:54 +0600

    positive and negative identification

---
+++ positive_negative.py
@@ -0,0 +1,12 @@
+num = int(input("enter a number: "))
+
+print("Number to be checked: " , num)
+
+if num > 0:
+    print("it is a positive number")
+
+elif num < 0:
+    print("it is a negative number")
+
+else: 
+    print("it is a neutral number")

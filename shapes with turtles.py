f06938d3939c86204939236d1d67a9d39b0d14ac
Mahdiya12 <mmahnoorshooter@gmail.com>
2025-10-13 13:05:47 +0600

    semi house with turtle

---
+++ shapes with turtles.py
@@ -0,0 +1,27 @@
+import turtle
+turtle.Screen().bgcolor("Lavender")
+board = turtle.Turtle()
+
+#tiragle
+board.forward(100)
+board.left(120)
+
+board.forward(100)
+board.left(120)
+
+board.forward(100)
+
+#sqaure
+
+board.left(30)
+board.forward(100)
+
+board.left(90)
+board.forward(100)
+
+board.left(90)
+board.forward(100)
+
+turtle.done()
+
+

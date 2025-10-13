8960d1211cfcc44a20a923a2a7dcdf5e2ecda82c
Mahdiya12 <mmahnoorshooter@gmail.com>
2025-10-13 11:51:43 +0600

    playing wth string

---
+++ play with strings.py
@@ -0,0 +1,16 @@
+first_name = "Codingal"
+last_name = "Educations"
+full_name = first_name+last_name
+example = "Haa"*5
+
+print("First name: " , first_name)
+print("lastname: " , last_name)
+print("Fullt name: " , full_name)
+print("String multiplied 5 times gives this result: " , example)
+
+word = 'Coding'
+print("Length of string:" , len(word))
+print("First letter of String: " , word[0])
+print("Last letter of string: " , word[5])
+print("String sliced: " , word[0:3])
+

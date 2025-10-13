3c72d9a9ec98ade7fce3844a6d33b6b1043f4bc8
Mahdiya12 <mmahnoorshooter@gmail.com>
2025-10-13 12:53:05 +0600

    conversatio with strings

---
+++ concersation_with_srtings.py
@@ -0,0 +1,18 @@
+first_name = "Mahdiya"
+last_name = "Mahnoor"
+full_name = first_name+last_name
+age = "12"*2
+is_student = True
+weight = 40.5
+
+print("First name: " , first_name)
+print("lastname: " , last_name)
+print("Fullt name: " , full_name)
+print("Age multiplied 2 times gives this result: " , age)
+
+word = 'Mahdiya'
+print("Length of string:" , len(word))
+print("First letter of String: " , word[0])
+print("Last letter of string: " , word[6])
+print("String sliced: " , word[3:7])
+

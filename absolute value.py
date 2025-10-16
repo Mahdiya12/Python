4084e7d9d504984b0c0b397191e8e66709b71499
Mahdiya12 <mmahnoorshooter@gmail.com>
2025-10-16 21:39:11 +0600

    valuing num

---
+++ absolute value.py
@@ -0,0 +1,9 @@
+def absolte_value(num):
+    
+    if num >= 0:
+        return num
+    else:
+        return -num
+    
+print("The absolute value of 89" , absolte_value(89))
+print("The absolute value of -57" , absolte_value(-57))

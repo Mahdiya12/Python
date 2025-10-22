50578c913d2d87ea262c9ddfea77ce6185b06582
Mahdiya12 <mmahnoorshooter@gmail.com>
2025-10-22 22:12:07 +0600

    parrot part 2

---
+++ parrot bird 3.py
@@ -0,0 +1,19 @@
+class parrot:
+
+   def __init__(self, name, age):
+  
+      self.name = name
+      self.age = age
+
+   def sing(self, song):
+    return "{} is singing {}".format(self.name , song)
+ 
+   def dance(self):
+    return "{} is dancing".format(self.name)
+ 
+blu = parrot("Blu" , 10)
+
+
+print(blu.sing("Rewrite the stars"))
+print(blu.dance())
+

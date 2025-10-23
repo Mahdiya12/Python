2454ea6e1e30aab740ac11d249baec91a2749e03
Mahdiya12 <mmahnoorshooter@gmail.com>
2025-10-23 21:41:23 +0600

    rec area

---
+++ area of rec.py
@@ -0,0 +1,14 @@
+class Rectangle(): 
+
+   def __init__(self, l, w):
+      self.length = l
+      self.width = w
+
+   def areaRectangle(self):
+
+      return self.length * self.width  
+   
+newRectangle = Rectangle(10, 20)   
+print("Dimension of rectangle widht = %d , length = %d " %(newRectangle.width , newRectangle.length)) 
+
+print("Area of rectangle is" , newRectangle.areaRectangle())

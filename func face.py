8768aa23f7b6c2941faddbc50b185e83738ef9b4
Mahdiya12 <mmahnoorshooter@gmail.com>
2025-10-26 11:47:53 +0600

    idk funcky face

--- func face.py
+++ func face.py
@@ -1,27 +1,68 @@
 from turtle import *
 
-class Face():
-    def __init__(self, xpos, ypos):
+# A function is defined for each part, with following steps
+# 1. pen up
+# 2. move to correct position
+# 3. pen down
+# 4. draw
+# 5. return home
 
-        self.size = 90
+class Face:
+    def __init__(self, xpos, ypos):
+        self.size = 50
         self.coord = (xpos, ypos)
-        self.nosesize = "small"
+        self.nosesize = 'small'
 
     def setSize(self, radius):
         self.size = radius
 
     def draw(self):
         self.goHome()
-        pensize(3)    
+        pensize(3)
         speed(0)
         self.drawOutline()
         self.drawEye(135)
-        self.drawEye(135)
-        self.drawEye(45)
+        self.drawEye(-135)
         self.drawMouth()
         self.drawNose()
-        pensize(5)
 
-    def goHome():
-        
+    # After drawing each part, turtle position
+    # returns to centre. Parts can be drawn in any order
+    def goHome(self):
+        penup()
+        goto(self.coord)
+        setheading(0)
 
+    def drawOutline(self):
+        penup()
+        # move turtle pen in forward direction
+        forward(self.size)
+        pendown()
+        # draw circle of given radius
+        pencolor('black')
+        circle(self.size)
+        # return back to centre
+        self.goHome()
+
+    def drawEye(self, turn):
+        penup()
+        # move turtle pen to given angle
+        left(turn)
+        # move turtle pen in forward direction
+        forward(self.size / 2)
+        pendown()
+        # draw circle of given radius
+        dot(self.size/10)
+        # return back to centre
+        self.goHome()
+
+    def drawMouth(self):
+        penup()
+        # move turtle pen to given angle
+        right(135)
+        # move turtle pen in forward direction
+        forward(self.size/1.7)
+        left(90)
+        pendown()
+        # draw arc of circle of given radius
+        # but till given extent only

37f8bf50c6fa697cbc90293a0275730d98acf085
Mahdiya12 <mmahnoorshooter@gmail.com>
2025-11-12 21:39:38 +0600

    texting 2

---
+++ file_handling_operation_2.py
@@ -0,0 +1,17 @@
+file = open('coding.txt' , 'r')
+
+print(file.read())
+
+file.close()
+
+file = open('coding.txt' , 'w')
+
+file.write("hello! this is Mahdiya and i'm an athlete!")
+
+file.close()
+
+file = open('coding.txt' , 'a')
+
+file.write("for sure i love playing different sports! although i do enjoy maths and science!")
+
+file.close()
